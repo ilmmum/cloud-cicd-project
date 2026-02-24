2c6fa8339898168241564857db4668f870502765
Oluwole Olajide <Oluwole.jide3@gmail.com>
2026-02-24 14:42:29 +0100

    Changed message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Cloud DevOps and GenAiOps is Power");
+    res.send("CI/CD is automatic");
 });
 app.listen(3000, () => {
     console.log("Server running on port 3000");

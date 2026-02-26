8025d9994fe666b08fc640ea8db8d47001da28e9
Oluwole Olajide <Oluwole.jide3@gmail.com>
2026-02-26 16:57:17 +0100

    Test CI/CD

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Cloud DevOps and GenAiOps is Power");
+    res.send("Testing if the CI/CD automate");
 });
 app.listen(3000, () => {
     console.log("Server running on port 3000");
